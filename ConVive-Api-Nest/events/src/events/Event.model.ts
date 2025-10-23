85075caf21ae2735f3e6da3962e4e9299a65827d
Azvedo-06 <azinho006@gmail.com>
2025-10-23 00:29:36 -0300

    Adiciona microserviço events com NestJS

---
+++ ConVive-Api-Nest/events/src/events/Event.model.ts
@@ -0,0 +1,25 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity({ name: 'events' })
+export class Event {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  date: Date;
+
+  @Column()
+  location: string;
+
+  @Column()
+  imageUrl: string;
+
+  @Column()
+  descriptionCard: string;
+
+  @Column()
+  descriptionModal: string;
+}
